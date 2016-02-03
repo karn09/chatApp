fc036f7a0103a3bfdb197c2d5ae57c28ec9469c9
Rob Debbane <studio@robertdebbane.com>
2016-02-02 21:28:38 -0500

    changes to exporting chatBank

--- chatBank.js
+++ chatBank.js
@@ -1,5 +1,12 @@
 data = [];
+var _ = require('lodash');
 
 function add(name, chatText) {
 	data.push({name: name, chatText: chatText});
 }
+
+function list(data) {
+	return _.cloneDeep(data);
+}
+
+module.exports = {add: add, list:list};
